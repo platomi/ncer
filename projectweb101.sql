3091ed52430b23bf9ef712a84847626c2ee4cea3
Rustom Cadenas <rdcadenas1@gmail.com>
2021-10-22 14:02:56 +0800

    22 10

--- projectweb101.sql
+++ projectweb101.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 18, 2021 at 09:54 AM
+-- Generation Time: Oct 22, 2021 at 08:01 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -31,8 +31,16 @@ CREATE TABLE `tbl_invitation` (
   `id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `status` int(2) NOT NULL
+  `status` int(2) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_invitation`
+--
+
+INSERT INTO `tbl_invitation` (`id`, `email`, `created_at`, `status`) VALUES
+(1, 'jrcrubenial@sdssu.edu.ph', '2021-10-21 14:32:37', 0),
+(2, 'rdcadenas@sdssu.edu.ph', '2021-10-21 15:36:06', 1);
 
 -- --------------------------------------------------------
 
@@ -61,7 +69,10 @@ CREATE TABLE `tbl_registers` (
 
 INSERT INTO `tbl_registers` (`id`, `id_`, `title`, `fullname`, `email`, `gender`, `mobile`, `affiliation`, `designation`, `created_at`, `updated_at`, `active`) VALUES
 (4, '616ce156c6eb7', 'fds', 'fdsf', 'rdcadenas1@gmail.com', 'Male', '3244324', 'fdsfds', 'ddsfdsfd', '', '', ''),
-(5, '616d27f321cdd', 'hello world', 'Bob Uy pig', 'rdcadenas1@gmail.com', 'Male', '09312321341412', 'fdsfdsf', 'dsfsdfsdfds', '', '', '');
+(5, '616d27f321cdd', 'hello world', 'Bob Uy pig', 'rdcadenas1@gmail.com', 'Male', '09312321341412', 'fdsfdsf', 'dsfsdfsdfds', '', '', ''),
+(6, '6171058f02cdc', 'fdsaf', 'Bob Uy pig', 'rdcadenas1@gmail.com', 'Male', '234242424', 'fsdfdsfsd', 'fdsfsdfs', '', '', ''),
+(7, '61710652b77ec', 'fdsfs', 'fsfds', 'rdcadenas1@gmail.com', 'Female', '3244324', 'fsfsfs', 'fs', '', '', ''),
+(8, '617119117a770', 'sadasds', 'sadsadsa', 'dsadsa@gmail.com', 'Male', '4324234324', 'fsdfsd', 'fsfsfs', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -119,13 +130,13 @@ ALTER TABLE `tbl_users`
 -- AUTO_INCREMENT for table `tbl_invitation`
 --
 ALTER TABLE `tbl_invitation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tbl_registers`
 --
 ALTER TABLE `tbl_registers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `tbl_users`
